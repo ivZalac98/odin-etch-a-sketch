d894cdcf9fa10c013d388b4a73db787090d73d0f
ivZalac98 <ivzalac@gmail.com>
2022-04-23 19:09:59 +0200

    Add reset button and organize code into functions

--- script.js
+++ script.js
@@ -1,38 +1,64 @@
-const ROW = 20;
-const COLUMN = 20;
-
-const container = document.createElement("div");
-container.setAttribute(
-  "style",
-  "display: flex; flex-direction: column; width: 800px; height: auto" //width: 90vw; height: 90vh
-);
-container.id = "container";
-document.body.appendChild(container);
-
-document.body.setAttribute(
-  "style",
-  "display: flex; justify-content: center; align-items: center"
-);
-
-for (let i = 0; i < ROW; i++) {
-  const row = document.createElement("div");
-  row.setAttribute("style", "display: flex; flex: 1");
-  row.setAttribute("id", `row${i}`);
-  for (let j = 0; j < COLUMN; j++) {
-    const column = document.createElement("div");
-    column.setAttribute("style", "flex: 1; aspect-ratio: 1/1");
-    column.setAttribute("id", `pix${i}x${j}`);
-    column.style.border = "1px dotted gray";
-    row.appendChild(column);
-  }
-  container.appendChild(row);
+function createContainer(containerId, width = "800px") {
+  const container = document.createElement("div");
+  container.setAttribute(
+    "style",
+    "display: flex; flex-direction: column; width: " + width + "; height: auto;"
+  );
+  container.id = containerId;
+  return container;
 }
 
-for (let i = 0; i < ROW; i++) {
-  for (let j = 0; j < COLUMN; j++) {
-    const column = document.querySelector(`#pix${i}x${j}`);
-    column.addEventListener("mouseover", () => {
-      column.style.backgroundColor = "gray";
-    });
+function createSketchpad(widthOfPad, containerId) {
+  for (let i = 0; i < widthOfPad; i++) {
+    const row = document.createElement("div");
+    row.setAttribute("style", "display: flex; flex: 1");
+    // row.setAttribute("id", `row${i}`);
+    for (let j = 0; j < widthOfPad; j++) {
+      const column = document.createElement("div");
+      column.setAttribute("style", "flex: 1; aspect-ratio: 1/1");
+      // column.setAttribute("id", `pix${i}x${j}`);
+      column.addEventListener("mouseover", () => {
+        column.style.backgroundColor = "gray";
+      });
+      column.style.border = "1px dotted gray";
+      row.appendChild(column);
+    }
+    const container = document.querySelector(`#${containerId}`);
+    container.appendChild(row);
   }
+
+  // for (let i = 0; i < ROW; i++) {
+  //   for (let j = 0; j < COLUMN; j++) {
+  //     const column = document.querySelector(`#pix${i}x${j}`);
+  //     column.addEventListener("mouseover", () => {
+  //       column.style.backgroundColor = "gray";
+  //     });
+  //   }
+  // }
+}
+
+//adds event to reset button
+function resetAndReinitialize(containerId = "container") {
+  const resetButton = document.querySelector("#reset");
+  resetButton.addEventListener("click", () => {
+    let widthOfPad = window.prompt(
+      "How wide do you want sketchpad to be? number between 1 and 100."
+    );
+    while (widthOfPad < 1 || widthOfPad > 100) {
+      widthOfPad = window.prompt("Bad bad user :) try again. 1 - 100");
+    }
+    const container = document.querySelector(`#${containerId}`);
+    container.remove();
+    initialize(widthOfPad);
+  });
+}
+
+function initialize(
+  widthOfPad = 20,
+  containerId = "container",
+  width = "800px"
+) {
+  document.body.appendChild(createContainer(containerId, width));
+  createSketchpad(widthOfPad, containerId);
+  resetAndReinitialize(containerId);
 }
